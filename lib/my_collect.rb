e8b065829a9416d470e80725154495b7a1ea4672
Timothy Lalli <trlalli92@gmail.com>
2020-07-15 15:41:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,9 @@
-def my_collection(collection)
+def my_collection(array)
   i = 0 
-  my_collect = []
   while i < array.length
     my_collect << yield(collection[i])
     i += 1 
   end
-  my_collect
 end
  
   
