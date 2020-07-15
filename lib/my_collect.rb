22937c9887008f5eec8f23a0ae67fd3794e4e72b
Timothy Lalli <trlalli92@gmail.com>
2020-07-15 15:57:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,11 +2,9 @@ def my_collect(array)
   i = 0
   new_array = []
   while i < array.length
-    do |name|
       name.split(" ").first 
       new_array << yield my_collect(array) 
       i += 1 
-    end
   end
 end
  
